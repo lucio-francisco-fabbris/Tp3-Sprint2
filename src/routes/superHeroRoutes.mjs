a797d9b3ff9696ad8036baaf001f17f413ee4fef
Lucio Francisco <franfa0610@gmail.com>
2026-03-20 18:20:04 -0300

    Fix route order for super hero search functionality

--- src/routes/superHeroRoutes.mjs
+++ src/routes/superHeroRoutes.mjs
@@ -4,8 +4,8 @@ import {obtenerSuperHeroePorIdController, obtenerTodosLosSuperHeroesController, 
 const router = express.Router();
 
 router.get('/heroes', obtenerTodosLosSuperHeroesController);
-router.get('/heroes/buscar/:atributo/:valor', buscarSuperHeroesPorAtributoController);
 router.get('/heroes/mayores-30', obtenerSuperHeroesMayoresDe30Controller);
+router.get('/heroes/buscar/:atributo/:valor', buscarSuperHeroesPorAtributoController);
 router.get('/heroes/:id', obtenerSuperHeroePorIdController);
 
 export default router;
